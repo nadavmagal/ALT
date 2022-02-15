a83ed736cdaf2ee40bf4f39ac51e5f6020bf540d
SofferLior <soffer.lior@gmail.com>
2022-02-15 22:07:32 +0200

    split config file

--- binary_problems.py
+++ binary_problems.py
@@ -1,6 +1,6 @@
 def tag_odd_even(labels):
     labels = labels % 2
-    labels[labels == 0 ] = -1
+    labels[labels == 0] = -1
     return labels
 
 
@@ -10,7 +10,19 @@ def tag_is_big_from_5(labels):
     return labels
 
 
+def tag_is_big_from_4(labels):
+    labels[labels >= 4] = 1
+    labels[labels < 4] = -1
+    return labels
+
+
 def tag_bd_date(labels):
     labels[(labels == 3) | (labels == 1) | (labels == 9) | (labels == 0)] = 1
+    labels[labels != 1] = -1
+    return labels
+
+
+def tag_divide_by_3(labels):
+    labels[(labels == 3) | (labels == 6) | (labels == 9) | (labels == 0)] = 1
     labels[labels != 1] = -1
     return labels
